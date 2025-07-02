65133fc15c32fcd4f2438cc379ee8a4acb0c7be0
bhullarravia <bhullarravia@gmail.com>
2025-07-02 15:50:51 +0000

    Variables Practice

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,23 @@
 
 #include <iostream>
 
+int a,b =5;
+
 int main(){
-    
+    bool myFlag;
+    a=7;
+    myFlag= false;
+    std::cout<<"a= " << a << std::endl;
+    std::cout<<"b= " << b << std::endl;
+    std::cout<<"myFlag= " << myFlag << std::endl;
+    myFlag=true;
+    std::cout << "myFlag= " << myFlag << std::endl;
+    std::cout << "a+b= " << a+b << std::endl;
+    std::cout << "b-a= " << b-a << std::endl;
+
+    unsigned int positive;
+    positive = b - a; // This will be a negative value, but stored as unsigned
+    std:: cout << "b-a (unsigned)= " << positive << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
